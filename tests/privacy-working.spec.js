5518aff48a649a6ed0310edb983d277c017ecff3
koushik-27 <nv344@njit.edu>
2023-03-28 17:14:21 -0400

    updated the test case such that it works as expected

--- tests/privacy-working.spec.js
+++ tests/privacy-working.spec.js
@@ -1,48 +1,45 @@
 const { test, expect } = require('@playwright/test');
 
-test('Agree button should update localStorage and hide the privacy modal', async ({ page }) => {
-  // Navigate to the page with the privacy modal
+test('Privacy Policy modal test', async ({ page }) => {
+  // Navigate to the homepage
   await page.goto('http://localhost:3000');
 
-  // Wait for the privacy modal to appear
-  await page.waitForSelector('#privacyModal', { timeout: 5000 });
+  // Check that the Privacy Policy modal appears
+  const privacyModalExists = await page.$('#privacyModal');
+  expect(privacyModalExists).toBeTruthy();
 
-  // Check if the privacy modal is visible
-  const privacyModal = await page.isVisible('#privacyModal');
-  expect(privacyModal).toBeTruthy();
+  // Click the Disagree button
+  await page.click('#privacyModal .modal-footer .btn-secondary');
 
-  // Click on the Agree button in the privacy modal
+  // Wait for the Privacy Policy modal to disappear
+  await page.waitForSelector('#privacyModal', { state: 'hidden' });
+
+  // Check that the Privacy Policy modal is hidden
+  const privacyModalHidden = await page.isHidden('#privacyModal', { timeout: 5000 });
+  expect(privacyModalHidden).toBeTruthy();
+
+  // Reload the page to start fresh
+  await page.reload();
+
+  // Check that the Privacy Policy modal appears again
+  const privacyModalExists2 = await page.$('#privacyModal');
+  expect(privacyModalExists2).toBeTruthy();
+
+  // Click the Agree button
   await page.click('#agreeButton');
 
-  // Check that localStorage has been updated
-  const agreed = await page.evaluate(() => localStorage.getItem('privacyPolicyAgreed'));
-  expect(agreed).toBe('true');
+  // Wait for the Privacy Policy modal to disappear
+  await page.waitForSelector('#privacyModal', { state: 'hidden' });
 
-  // Check that the privacy modal is hidden
-  //const privacyModalHidden = await page.isHidden('#privacyModal', { timeout: 5000 });
-  //expect(privacyModalHidden).toBeTruthy();
+  // Check that the Privacy Policy modal is hidden
+  const privacyModalHidden2 = await page.isHidden('#privacyModal', { timeout: 5000 });
+  expect(privacyModalHidden2).toBeTruthy();
+
 });
 
-test('Disagree button should not update localStorage and hide the privacy modal', async ({ page }) => {
-  // Navigate to the page with the privacy modal
-  await page.goto('http://localhost:3000');
 
-  // Wait for the privacy modal to appear
-  await page.waitForSelector('#privacyModal', { timeout: 5000 });
 
-  // Check if the privacy modal is visible
-  const privacyModal = await page.isVisible('#privacyModal');
-  expect(privacyModal).toBeTruthy();
 
-  // Click on the Disagree button in the privacy modal
-  await page.click('text=Disagree');
 
-  // Check that localStorage has not been updated
-  const agreed = await page.evaluate(() => localStorage.getItem('privacyPolicyAgreed'));
-  expect(agreed).toBeNull();
 
-  // Check that the privacy modal is hidden
-  //const privacyModalHidden = await page.isHidden('#privacyModal', { timeout: 5000 });
-  //expect(privacyModalHidden).toBeTruthy();
-});
 
